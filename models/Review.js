69d47fa9dccb0ce15e5b7902fb40c7a708f14e84
SaraMsoso <Sara.soder44@gmail.com>
2025-06-24 15:51:16 +0200

    Make menuItemId optional in Review schema to allow general homepage reviews

--- models/Review.js
+++ models/Review.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Define schema for reviews submitted by users
 const reviewSchema = new mongoose.Schema({
-  menuItemId: { type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: true },
+  menuItemId: { type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: false },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
   rating: { type: Number, required: true, min: 1, max: 5 },
