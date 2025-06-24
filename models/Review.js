53c2e14abea31c70215a4feba83f5091b5a21dfc
SaraMsoso <Sara.soder44@gmail.com>
2025-06-24 16:54:26 +0200

    Added deafult null in menuItemId and userId

--- models/Review.js
+++ models/Review.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // Define schema for reviews submitted by users
 const reviewSchema = new mongoose.Schema({
-  menuItemId: { type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: false },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  menuItemId: { type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: false, default: null },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false, default: null },
   name: { type: String, required: true },
   rating: { type: Number, required: true, min: 1, max: 5 },
   comment: { type: String, required: true },
